471f78777acc3734692cf42d734fa1f379756043
Danil <dfarsal@ya.ru>
2021-04-22 01:50:49 +0300

    added task3

---
+++ Homework 21.04/task3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int BinSearch(int *M, int n, int k)
+{
+    int L = 0;
+    int R = n;
+
+    int m;
+    int z = 0;
+
+    while (L < R)
+    {
+        z++;
+        m = (L + R) / 2;
+        if (k > M[m])
+            L = m;
+        if (k < M[m])
+            R = m;
+        if (k == M[m])
+            break;
+        if (R - L <= 1)
+        {
+            break;
+        }
+    }
+
+    return m;
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+    int size = x / 2 + 2;
+    int quads[size];
+    for (int i = 0; i < size; i++)
+    {
+        quads[i] = i * i;
+    }
+
+    int square_index = BinSearch(quads, size, x);
+    cout << square_index << endl;
+
+    return 0;
+}
